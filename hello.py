cd626cb23c79b5024ae013de56a7ea667f8d66d9
Mingming Li <mingmingli916@gmail.com>
2022-10-12 22:06:38 -0400

    Response.

--- hello.py
+++ hello.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return redirect('http://www.example.com')
 
 
 @app.route('/user/<name>')
