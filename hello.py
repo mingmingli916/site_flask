9a129d413939acf1f06c8d3dff54986629153cd3
Mingming Li <mingmingli916@gmail.com>
2022-10-13 19:08:05 -0400

    SQLAlchemy relationships.

--- hello.py
+++ hello.py
@@ -56,6 +56,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
 
     def __repr__(self):
         return f'<Role {self.name}>'
@@ -65,6 +66,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     # For better debugging and testing
     def __repr__(self):
