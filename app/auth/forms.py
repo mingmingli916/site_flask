681ffc0f600c9768bf7b0911cb7c89a8e4a90c2f
Mingming Li <mingmingli916@gmail.com>
2022-10-14 12:18:18 -0400

    Password updates.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -33,3 +33,11 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password',
+                             validators=[DataRequired(), EqualTo('password2', message='Passwords not match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Update password')
