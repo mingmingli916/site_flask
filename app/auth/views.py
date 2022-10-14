ca65ce3d1af8c6d5bd4c67b09991a9b06f72d00c
Mingming Li <mingmingli916@gmail.com>
2022-10-14 09:49:43 -0400

    Signing Users In.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_ = request.args.get('next')
+            if next_ is None or not next_.startswith('/'):
+                next_ = url_for('main.index')
+            return redirect(next_)
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
