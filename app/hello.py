70f57655032246c5ed24065ac5886c2d20d7f27c
Mingming Li <mingmingli916@gmail.com>
2022-10-14 00:12:11 -0400

    Configuration Options.

--- app/hello.py
+++ app/hello.py
@@ -13,7 +13,7 @@ from threading import Thread
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')  # flask-alchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../data.sqlite')  # flask-alchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'I am Mingming Li'  # flask-wtf
 # flask-mail
