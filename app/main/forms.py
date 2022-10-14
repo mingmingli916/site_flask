7622de69c85e493dd5d8744b2de78bdf67f925dc
Mingming Li <mingmingli916@gmail.com>
2022-10-14 16:55:08 -0400

    Profile Editor.

---
+++ app/main/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Length, DataRequired, Email, Regexp, ValidationError
+from ..models import Role, User
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1, 64),
+        Regexp('^[a-zA-Z][a-zA-Z0-9_.]*$', 0,
+               'Usernames must have only letters, numbers, dots or underscores.')
+    ])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user  # save it to be used for other validate method.
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email has been registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
