eb1d3d3f5a43eb9c21e24b2b0d7e07cbef004404
Mingming Li <mingmingli916@gmail.com>
2022-10-15 17:21:10 -0400

    Error Handling.

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, render_template, request
 from app.exceptions import ValidationError
 from . import api
 
@@ -24,3 +24,21 @@ def forbidden(message):
 @api.errorhandler(ValidationError)
 def validation_error(e):
     return bad_request(e.args[0])
+
+
+@api.errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
+
+
+@api.errorhandler
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
+    return render_template('500.html'), 500
