d591a749f23fde85891f9543793f4c550758e2b8
Mingming Li <mingmingli916@gmail.com>
2022-10-16 09:42:21 -0400

    add unix config for logging into syslog.

--- config.py
+++ config.py
@@ -74,11 +74,25 @@ class DockerConfig(ProductionConfig):
         app.logger.addHandler(file_handler)
 
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.INFO)
+        app.logger.addHandler(syslog_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'docker': DockerConfig,
+    'unix': UnixConfig,
 
     'default': DevelopmentConfig
 }
